45ce30e25b7110c2255910aa9fa01914d369a38f
frankwoods <liamtbo@gmail.com>
2025-07-09 21:20:29 -0700

    implemented entire def rm_data_prior_to_arm_pen functions, not tested yet

--- test.py
+++ test.py
@@ -1,67 +1,90 @@
-# Authors: The scikit-learn developers
-# SPDX-License-Identifier: BSD-3-Clause
+import pandas as pd
+import unittest
 
-from time import time
+# def find_curve_start_idx(df):
+#     # stores range indexes as (start, end) tuple pairs
+#     ranges_above_zero_list = []
+#     range_max_height_list = []
+#     range_start_idx = 0
+#     range_end_idx = 0
+#     range_max_resistance = 0
 
-import numpy as np
-from matplotlib import pyplot as plt
+#     for i, res in enumerate(df["resistance"]):
+#         if res > 0: 
+#             range_end_idx = i
+#             range_max_resistance = max(range_max_resistance, res)
+#             if i == len(df["resistance"]) - 1:
+#                 ranges_above_zero_list.append((range_start_idx, range_end_idx))
+#                 range_max_height_list.append(range_max_resistance)
+#         elif res == 0 or i == len(df["resistance"]) - 1:
+#             # if we've actually captured a range
+#             if range_end_idx - range_start_idx > 0:
+#                 # updata parallel arrays
+#                 ranges_above_zero_list.append((range_start_idx, range_end_idx))
+#                 range_max_height_list.append(range_max_resistance)
+#                 # reset variables
+#                 range_start_idx = i
+#                 range_end_idx = i
+#                 range_max_resistance = 0
+#             else:
+#                 range_start_idx = i
+#                 range_end_idx = i
+#     return ranges_above_zero_list, range_max_height_list
 
-from sklearn import datasets, manifold
+def find_curve_start_idx(df):
+    ranges_above_zero_list = []
+    range_max_height_list = []
 
-digits = datasets.load_digits()
-X, y = digits.data, digits.target
-n_samples, n_features = X.shape
+    in_range = False
+    range_start_idx = None
+    range_max_resistance = 0
 
-np.random.seed(0)
+    for i, res in enumerate(df["resistance"]):
+        if res > 0:
+            if not in_range:
+                # starting a new range
+                in_range = True
+                range_start_idx = i - 1
+                range_max_resistance = res
+            else:
+                range_max_resistance = max(range_max_resistance, res)
+        elif in_range:
+            # end of a positive range
+            ranges_above_zero_list.append((range_start_idx, i - 1))
+            range_max_height_list.append(range_max_resistance)
+            in_range = False
 
+    # handle if last element was part of a range
+    if in_range:
+        ranges_above_zero_list.append((range_start_idx, len(df["resistance"]) - 1))
+        range_max_height_list.append(range_max_resistance)
 
-# ----------------------------------------------------------------------
-# Visualize the clustering
-def plot_clustering(X_red, labels, title=None):
-    x_min, x_max = np.min(X_red, axis=0), np.max(X_red, axis=0)
-    # scales
-    X_red = (X_red - x_min) / (x_max - x_min)
-
-    plt.figure(figsize=(6, 4))
-    for digit in digits.target_names:
-        print(f"digit target name: {digit}")
-        plt.scatter(
-            *X_red[y == digit].T,
-            marker=f"${digit}$",
-            s=50,
-            c=plt.cm.nipy_spectral(labels[y == digit] / 10),
-            alpha=0.5,
-        )
-
-    plt.xticks([])
-    plt.yticks([])
-    if title is not None:
-        plt.title(title, size=17)
-    plt.axis("off")
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-
-
-# ----------------------------------------------------------------------
-# 2D embedding of the digits dataset
-print("Computing embedding")
-X_red = manifold.SpectralEmbedding(n_components=2).fit_transform(X)
-print("Done.")
-
-from sklearn.cluster import AgglomerativeClustering
-linkage = "ward"
-clustering = AgglomerativeClustering(linkage=linkage, n_clusters=10)
-t0 = time()
-clustering.fit(X_red)
-print("%s :\t%.2fs" % (linkage, time() - t0))
-print(X_red)
-plot_clustering(X_red, clustering.labels_, "%s linkage" % linkage)
-# for linkage in ("ward", "average", "complete", "single"):
-#     clustering = AgglomerativeClustering(linkage=linkage, n_clusters=10)
-#     t0 = time()
-#     clustering.fit(X_red)
-#     print("%s :\t%.2fs" % (linkage, time() - t0))
-
-#     plot_clustering(X_red, clustering.labels_, "%s linkage" % linkage)
+    return ranges_above_zero_list, range_max_height_list
 
 
-plt.show()
+class test(unittest.TestCase):
+
+    def test_1(self):
+        df = pd.DataFrame({"resistance": [0, 1, 2, 3, 2, 1, 0]})
+        self.assertEqual(find_curve_start_idx(df), ([(0,5)], [3]))
+    def test_2(self):
+            df = pd.DataFrame({"resistance": [0, 1, 0, 2, 0]})
+            self.assertEqual(find_curve_start_idx(df), ([(0,1),(2,3)], [1,2]))
+    def test_3(self):
+            df = pd.DataFrame({"resistance": [0, 0, 1, 0, 0, 2, 0, 0]})
+            self.assertEqual(find_curve_start_idx(df), ([(1,2),(4,5)], [1,2]))
+    def test_4(self):
+        df = pd.DataFrame({"resistance": [0, 0, 1, 0, 0, 2, 0, 9, 5]})
+        self.assertEqual(find_curve_start_idx(df), ([(1,2),(4,5),(6,8)], [1,2,9]))
+    def test_5(self):
+        df = pd.DataFrame({"resistance": [5,8]})
+        self.assertEqual(find_curve_start_idx(df), ([(0,1)], [8]))
+    def test_6(self):
+        df = pd.DataFrame({"resistance": [1]})
+        self.assertEqual(find_curve_start_idx(df), ([(0,0)], [1]))
+    def test_7(self):
+        df = pd.DataFrame({"resistance": [0,0,0,0,0]})
+        self.assertEqual(find_curve_start_idx(df), ([], []))
+
+if __name__ == '__main__':
+    unittest.main()
