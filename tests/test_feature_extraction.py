bb845299c0b8aff56792cc28a84e9bcb1b673815
frankwoods <liamtbo@gmail.com>
2025-07-21 19:27:20 -0700

    fixed bug in def find_force_drop_subranges where the trough would be updated as we come off the bottom, with trough value getting higher and higher

--- tests/test_feature_extraction.py
+++ tests/test_feature_extraction.py
@@ -8,16 +8,20 @@ def find_force_drop_subranges(df: pd.DataFrame, ratio: float):
     curr_peak = 0
     curr_peak_idx = 0
     curr_trough_idx = 0
+    in_subrange = 0
     for idx in range(1, len(df['resistance'])):
 
         if df['resistance'].iloc[idx] >= curr_peak and curr_trough_idx != curr_peak_idx:
             down_moves_subrange_list.append((curr_peak_idx, curr_trough_idx))
+            # in_subrange = 0
 
+        # if df['resistance'].iloc[idx] >= curr_peak and not in_subrange:
         if df['resistance'].iloc[idx] >= curr_peak:
             curr_peak = df['resistance'].iloc[idx]
             curr_peak_idx = idx
             curr_trough_idx = idx
-        elif curr_peak - df['resistance'].iloc[idx] >= resistance_max * ratio: 
+        elif curr_peak - df['resistance'].iloc[idx] >= resistance_max * ratio and df['resistance'].iloc[idx] < df['resistance'].iloc[curr_trough_idx]: 
+            # in_subrange = 1
             curr_trough_idx = idx
             
     return down_moves_subrange_list
@@ -45,6 +49,9 @@ class test_find_force_drop_subranges(unittest.TestCase):
     def test_7(self):
         df = pd.DataFrame({'resistance': [0,1,2,1,2,3,4,5,6,5,6,7,8,9,10]})
         self.assertEqual(find_force_drop_subranges(df, 0.1), [(2,3),(8,9)])
+    def test_8(self):
+        df = pd.DataFrame({'resistance': [0,1,2,3,4,2,3,10]})
+        self.assertEqual(find_force_drop_subranges(df, 0.1), [(4,5)])    
 
 def find_largest_force_drop(df: pd.DataFrame, subrange_list: List[Tuple]):
     curr_max = 0
