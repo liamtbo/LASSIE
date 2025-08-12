c0f83bf36e2ab1edbe332e072742a25237b14718
frankwoods <liamtbo@gmail.com>
2025-08-12 10:51:19 -0700

    need to scrap almost everything, going with a line of best fit approach

--- tests/test_field_work/test_find_drops_plateaus.py
+++ tests/test_field_work/test_find_drops_plateaus.py
@@ -40,7 +40,26 @@ class Test_Find_Subrange_End_Idx(unittest.TestCase):
         subrange = pd.Series([5,5,5,5,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5])
         self.assertEqual(find_subrange_end_idx(subrange), len(subrange)-1)
 
+'''
+threshold(a, depth, res_start_value)
+    return a * depth + res_start_value
 
+in_subrange = 0
+for i in 1 to n resistance values
+    if not in subrange:
+        if val_i <= local_threshold(val_i-1):
+            in_subrange = 1
+            start_value = val_i-1
+    elif in_subrange:
+        # case: subrange has potential to end here
+        if val_i is above start_value:
+            if val_i > global_threshold(val_i-1):
+                in_subrange = 0
+                ... add subrange if larger then len depth
+            if val_i > local_threshold(subrange_max_value):
+                in_subrange = 0
+                ... add subrange if larger then len depth
+'''
 
 def find_drops_plateaus(df: pd.DataFrame, subrange_upper_bound_percent: float, subrange_min_length_percent: float) -> List[Tuple[int, int]]:
     in_subrange = 0
@@ -50,34 +69,35 @@ def find_drops_plateaus(df: pd.DataFrame, subrange_upper_bound_percent: float, s
     subrange_upper_bound = res.max() * subrange_upper_bound_percent
     dep = df['depth']
     subrange_min_length = dep.max() * subrange_min_length_percent
-    print(f'subrange_upper_bound: {subrange_upper_bound}')
+    # print(f'subrange_upper_bound: {subrange_upper_bound}')
 
     for idx in range(1, len(res)):
         # if not in_subrange: subrange_start_idx = idx
         if in_subrange: above_threshold = res[idx] > res[subrange_start_idx] + subrange_upper_bound
         else: above_threshold = res[idx] > res[idx - 1] + subrange_upper_bound
 
-        print(f'idx value: {res[idx]}')
+
+        # print(f'idx value: {res[idx]}')
         if not above_threshold and not in_subrange:
-            print('\tnot above_threshold and not in_subrange')
-            print('\tenter subrange')
+            # print('\tnot above_threshold and not in_subrange')
+            # print('\tenter subrange')
             in_subrange = 1
             subrange_start_idx = idx - 1
 
         if above_threshold and in_subrange:
-            print('\tabove_threshold and in_subrange')
+            # print('\tabove_threshold and in_subrange')
             in_subrange = 0
-            print('\texit subrange')
+            # print('\texit subrange')
             subrange_end_idx = idx - 1 # -1 bc above threshold is a breakout of subrange, thus isn't a part of it
             
             subrange_long_enough = dep[subrange_end_idx] - dep[subrange_start_idx] + 1 > subrange_min_length # +1 bc is range is 6-4, that's actually a len of 3 subrange not 2
             if subrange_long_enough: 
-                print('\tsubrange_long_enough')
+                # print('\tsubrange_long_enough')
                 subrange_list.append((subrange_start_idx, subrange_end_idx))
 
             previous_idx_is_start_of_next_subrange = res[idx] <= res[subrange_end_idx] + subrange_upper_bound
             if previous_idx_is_start_of_next_subrange:
-                print('\tprevious_idx_start_of_subrange')
+                # print('\tprevious_idx_start_of_subrange')
                 in_subrange = 1
                 subrange_start_idx = idx - 1
 
