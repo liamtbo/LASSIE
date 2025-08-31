30f394d83023f774225c0fc6bc40cf23e4b58d8b
frankwoods <liamtbo@gmail.com>
2025-08-30 22:07:58 -0700

    added in quarter slopes and fixed them to NOT use quartiel. also hardcoded plotting for feature extraction

--- plotting/plotting_feature_extraction.py
+++ plotting/plotting_feature_extraction.py
@@ -41,18 +41,6 @@ def find_force_drop_subranges(df: pd.DataFrame, percent_of_max_resistance: float
             
     return down_moves_subrange_list
 
-# def find_largest_force_drop(df: pd.DataFrame, subrange_list: List[Tuple]):
-#     curr_max_val = 0
-#     curr_max_level = 0
-#     curr_max_idxs = (-1,-1)
-#     for subrange_start, subrange_end in subrange_list:
-#         subrange_diff = df['resistance'].iloc[subrange_start] - df['resistance'].iloc[subrange_end]
-#         if subrange_diff > curr_max_val: 
-#             curr_max_val = subrange_diff
-#             curr_max_level = df['resistance'].iloc[subrange_start]
-#             curr_max_idxs = (subrange_start, subrange_end)
-#     return curr_max_val, curr_max_level, curr_max_idxs
-
 def find_largest_force_drop(df: pd.DataFrame, subrange_list: List[Tuple]):
     curr_max_drop_size = 0
     curr_max_subrange_idxs = (0,0)
@@ -91,26 +79,56 @@ def handle_curve_shape(curve_data:pd.DataFrame, ax):
     ax.plot(curve_data['depth'], curve_data['resistance'])
     ax.plot([0,curve_data['depth'].max()], [0,curve_data['resistance'].iloc[curve_data['depth'].values.argmax()]], color='red')
 
-def handle_first_third_slope(curve_data:pd.DataFrame, ax):
-    pass
+def handle_first_quarter_slope(curve_data:pd.DataFrame, ax):
+    ax.plot(curve_data['depth'], curve_data['resistance'])
+    res = curve_data['resistance']
+    dep = curve_data['depth']
+    ax.plot([0,dep.quantile(0.33)], [0,res.quantile(0.33)], color='red')
 
-def handle_second_third_slope(curve_data:pd.DataFrame, ax):
-    pass
+def handle_second_quarter_slope(curve_data:pd.DataFrame, ax):
+    ax.plot(curve_data['depth'], curve_data['resistance'])
+    res = curve_data['resistance']
+    dep = curve_data['depth']
+    ax.plot([dep.quantile(0.33),dep.quantile(0.66)], [res.quantile(0.33),res.quantile(0.66)], color='red')
 
-def handle_third_third_slope(curve_data:pd.DataFrame, ax):
-    pass
+def handle_third_quarter_slope(curve_data:pd.DataFrame, ax):
+    ax.plot(curve_data['depth'], curve_data['resistance'])
+    res = curve_data['resistance']
+    dep = curve_data['depth']
+    ax.plot([dep.quantile(0.66),dep.max()], [res.quantile(0.66),res.max()], color='red')
+
+def handle_fourth_quarter_slope(curve_data:pd.DataFrame, ax):
+    ax.plot(curve_data['depth'], curve_data['resistance'])
+    res = curve_data['resistance']
+    dep = curve_data['depth']
+    ax.plot([dep.quantile(0.66),dep.max()], [res.quantile(0.66),res.max()], color='red')
+
 
 def handle_q1(curve_data:pd.DataFrame, ax):
-    pass
+    ax.plot(curve_data['depth'], curve_data['resistance'])
+    res = curve_data['resistance']
+    dep = curve_data['depth']
+    q1_res = res.quantile(0.25)
+
+    ax.plot([0,dep.max()], [q1_res, q1_res], color='red')
 
 def handle_q3(curve_data:pd.DataFrame, ax):
-    pass
+    ax.plot(curve_data['depth'], curve_data['resistance'])
+    res = curve_data['resistance']
+    dep = curve_data['depth']
+    q3_res = res.quantile(0.75)
+    ax.plot([0,dep.max()], [q3_res, q3_res], color='red')
 
-def plot_feature_selection(curves_data:List[pd.DataFrame], feature_to_plot_idx:dict[str:List[int]]):
+def plot_feature_selection(feature_names:List[str], curves_data:List[pd.DataFrame], plot_idx:int):
     # find dims of plot
-    plot_xdim, plot_ydim = find_num_subplots(len(feature_to_plot_idx.keys()))
-    if plot_xdim < plot_ydim: figsize=(10,6)
-    else: figsize=(10,10)
+    # TODO un-hardcode this
+    # plot_xdim, plot_ydim = find_num_subplots(len(feature_names))
+    plot_xdim = 5
+    plot_ydim = 3
+    # if plot_xdim < plot_ydim: figsize=(10,6)
+    # else: figsize=(10,10)
+    figsize=(6,10)
+
     # normalize the x and y axis for every subplot
     all_depth_resistance_data = pd.concat(curves_data, axis=0, ignore_index=True)
     gloabl_max_depth = all_depth_resistance_data['depth'].max()
@@ -118,31 +136,45 @@ def plot_feature_selection(curves_data:List[pd.DataFrame], feature_to_plot_idx:d
     
     fig, axs = plt.subplots(plot_xdim, plot_ydim, figsize=figsize)
     flattened_axs = axs.flatten()
-    for feature_i, feature_name in enumerate(feature_to_plot_idx.keys()): # loop over subplots
+    for feature_i, feature_name in enumerate(feature_names): # loop over subplots
         ax = flattened_axs[feature_i]
         ax.set_xlim([0,gloabl_max_depth])
         ax.set_ylim([0,gloabl_max_resistance])
-        font_size = 12
+        font_size = 8
         ax.set_xlabel('Depth (m)', fontsize=font_size)
         ax.set_ylabel('Resistance (N)', fontsize=font_size)
         ax.set_title(feature_name.title(), fontsize=font_size)
 
+        curve_data = curves_data[plot_idx]
         if feature_name == "max_depth": 
-            curve_data = curves_data[feature_to_plot_idx['max_depth'][0]]
             handle_max_depth(curve_data, ax)
         elif feature_name == "max_resistance":
-            curve_data = curves_data[feature_to_plot_idx['max_resistance'][0]]
             handle_max_resistance(curve_data, ax)
         elif feature_name == "num_peaks":
             ax.set_title(f'{feature_name.title()} (Normalized)', fontsize=font_size)
-            curve_data = curves_data[feature_to_plot_idx['num_peaks'][0]]
             handle_num_peaks(curve_data, ax)
         elif feature_name == "largest_force_drop":
-            curve_data = curves_data[feature_to_plot_idx['largest_force_drop'][0]]
             handle_largest_force_drop(curve_data, ax)
         elif feature_name == "curve_shape":
-            curve_data = curves_data[feature_to_plot_idx['curve_shape'][0]]
             handle_curve_shape(curve_data, ax)
+        # elif feature_name == "first_quarter_slope":
+        #     curve_data = curves_data[feature_to_plot_idx["first_quarter_slope"][0]]
+            # handle_first_third_slope(curve_data, ax)
+        # elif feature_name == "second_quarter_slope":
+        #     curve_data = curves_data[feature_to_plot_idx["second_quarter_slope"][0]]
+        #     # handle_second_third_slope(curve_data, ax)
+        # elif feature_name == "third_quarter_slope":
+        #     curve_data = curves_data[feature_to_plot_idx["third_quarter_slope"][0]]
+        #     # handle_third_third_slope(curve_data, ax)
+        # elif feature_name == "fourth_quarter_slope":
+        #     curve_data = curves_data[feature_to_plot_idx["fourth_quarter_slope"][0]]
+            # handle_third_third_slope(curve_data, ax)
+        # elif feature_name == "q1":
+        #     curve_data = curves_data[feature_to_plot_idx["q1"][0]]
+        #     handle_q1(curve_data, ax)
+        # elif feature_name == "q3":
+        #     curve_data = curves_data[feature_to_plot_idx["q3"][0]]
+        #     handle_q3(curve_data, ax)
         else:
             print(f'feature name {feature_name} is not an extracted feature')
     plt.tight_layout()
